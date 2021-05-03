262f40b439732cfff78ce94d27c4ea0fa4d7b25d
Anon Karytski <heyheyjude23@gmail.com>
2021-05-03 21:57:40 +0300

    feat: add matrix and matrix render

---
+++ app/matrix.js
@@ -0,0 +1,8 @@
+const RED = "red";
+const BLACK = "black";
+
+const RBR = [RED, BLACK, RED];
+const BRB = [BLACK, RED, BLACK];
+const BBR = [BLACK, BLACK, RED];
+
+const MATRIX = [RBR, BRB, RBR, BBR, BRB, RBR, RBR, BRB, RBR, BBR, BRB, RBR];
